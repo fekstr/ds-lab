dab194c3bf2d87bcb90584472d9510ea2b5b6e31
Bartek <bartoszrudnikowicz840@gmail.com>
2022-10-15 17:06:10 +0200

    Generic image classification model class

--- code/main.py
+++ code/main.py
@@ -1,6 +1,16 @@
 from utils import train_model
 from models.resnet50 import ResNet50
+from models.pretrained_classification_model import ImgClassificationModel
+from torchvision.models import VGG19_Weights, vgg19
 import pytorch_lightning as pl
 
 pl.seed_everything(42)
-train_model(ResNet50, "CRC-VAL-HE-7K")
+
+# ResNet50
+# train_model(ResNet50(), "CRC-VAL-HE-7K")
+
+
+# VGG-19 or any different pre-trained image classification model from torchvision
+trained_vgg_19 = train_model(
+    ImgClassificationModel(vgg19, VGG19_Weights), train_batch_size=8
+)
--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from patch_dataset import PatchDataset
 from models.resnet50 import ResNet50
 from torch.utils.data import DataLoader, random_split
@@ -6,22 +7,30 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import pathlib
 
 
-def train_model(model: pl.LightningModule, data_path: str = "CRC-VAL-HE-7K"):
+def train_model(
+    model: pl.LightningModule,
+    data_path: str = "CRC-VAL-HE-7K",
+    train_batch_size: int = 32,
+    val_batch_size: int = 64,
+    max_epochs: int = 10,
+) -> pl.LightningModule:
     current_path = pathlib.Path(__file__).parent.resolve()
-    model = model()
+    model = model
     dataset = PatchDataset(current_path.parents[0].joinpath("data").joinpath(data_path))
     train_ds, val_ds = random_split(
         dataset=dataset,
         lengths=[int(0.8 * len(dataset)), int(len(dataset) - (0.8 * len(dataset)))],
     )
-    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_ds, batch_size=train_batch_size, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=val_batch_size, shuffle=False)
 
     trainer = pl.Trainer(
         gpus=-1,
-        max_epochs=10,
+        max_epochs=max_epochs,
         callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
         default_root_dir=pathlib.Path(__file__).parents[1].joinpath("saved_models"),
     )
 
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    return model
