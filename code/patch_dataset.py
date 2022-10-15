ad0502aaad4b96f5764633727c02c73a4fb1ec82
Bartek <bartoszrudnikowicz840@gmail.com>
2022-10-15 17:55:33 +0200

    Augmentation added

--- code/patch_dataset.py
+++ code/patch_dataset.py
@@ -3,7 +3,13 @@ from typing import Tuple
 from PIL import Image
 from torch import Tensor
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor, Normalize
+from torchvision.transforms import (
+    ToTensor,
+    Normalize,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+)
+import torch
 
 
 class PatchDataset(Dataset):
@@ -11,13 +17,20 @@ class PatchDataset(Dataset):
         self,
         dataset_path: str,
         transform=Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        augmentation=True,
     ) -> None:
         self.path = pathlib.Path(dataset_path)
         self.classes_folder = sorted(self.path.glob("*"))
         self.transform = transform
-        print(self.path)
         self.classes_map = {}
         self.imgs_paths = []
+
+        if augmentation:
+            self.augmentation = torch.nn.Sequential(
+                RandomVerticalFlip(0.5), RandomHorizontalFlip(0.5)
+            )
+        else:
+            self.augmentation = None
 
         for i, dir in enumerate(self.classes_folder):
             self.classes_map[dir.name] = i
@@ -34,5 +47,8 @@ class PatchDataset(Dataset):
         image = ToTensor()(image)
         if self.transform:
             image = self.transform(image)
+
+        if self.augmentation:
+            image = self.augmentation(image)
 
         return image, self.classes_map[img_path.parents[0].name]
