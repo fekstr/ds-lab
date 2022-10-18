97afd550777468027245f2e6893b6c0f1aa08f14
kksniak <kksniak@gmail.com>
2022-10-18 19:37:49 +0200

    format preprocess svs

--- code/deepStroma/preprocess_svs_image.py
+++ code/deepStroma/preprocess_svs_image.py
@@ -11,52 +11,54 @@ import numpy as np
 TARGET_MPP = 0.5
 TARGET_DIM = 1500
 
+
 class PreprocessingSVS:
-    
-    def __init__(self, image_path, target_path = None) -> None: 
-        #read slide   
+    def __init__(self, image_path, target_path=None) -> None:
+        # read slide
         slide = openslide.OpenSlide(image_path)
 
         # keep only best slide for mpp resampling
-        self.scale_factor = float(slide.properties['openslide.mpp-x'])/TARGET_MPP
+        self.scale_factor = float(slide.properties["openslide.mpp-x"]) / TARGET_MPP
         self.slide_dim = slide.dimensions
         level = slide.get_best_level_for_downsample(self.scale_factor)
-        self.image = slide.read_region((0, 0), level, slide.level_dimensions[level]).convert("RGB")
+        self.image = slide.read_region(
+            (0, 0), level, slide.level_dimensions[level]
+        ).convert("RGB")
 
         del slide
 
-
-    def resize_to_target_mpp(self):
+    def resize_to_target_mpp(self) -> None:
         new_x = math.floor(self.slide_dim[0] / self.scale_factor)
         new_y = math.floor(self.slide_dim[1] / self.scale_factor)
         self.image = self.image.resize((new_x, new_y), PIL.Image.BICUBIC)
 
-
-    def crop(self):
+    def crop(self) -> None:
         if self.image.size[0] > TARGET_DIM and self.image.size[1] > TARGET_DIM:
-            offset_x = round((self.image.size[0] - TARGET_DIM)/2)
-            offset_y = round((self.image.size[1] - TARGET_DIM)/2)
-            self.image = self.image.crop((offset_x, offset_y, offset_x + TARGET_DIM, offset_y + TARGET_DIM))
+            offset_x = round((self.image.size[0] - TARGET_DIM) / 2)
+            offset_y = round((self.image.size[1] - TARGET_DIM) / 2)
+            self.image = self.image.crop(
+                (offset_x, offset_y, offset_x + TARGET_DIM, offset_y + TARGET_DIM)
+            )
         else:
-            print('WRONG IMAGE DIMENSION... will skip this image')
+            print("WRONG IMAGE DIMENSION... will skip this image")
 
-    def normalise(self):
-        T = transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: x*255)])
-        torch_normaliser = torchstain.normalizers.MacenkoNormalizer(backend='torch')
-        target = PIL.Image.open('./Ref.png')
+    def normalise(self) -> None:
+        T = transforms.Compose(
+            [transforms.ToTensor(), transforms.Lambda(lambda x: x * 255)]
+        )
+        torch_normaliser = torchstain.normalizers.MacenkoNormalizer(backend="torch")
+        target = PIL.Image.open("./Ref.png")
         torch_normaliser.fit(T(target))
         norm, _, _ = torch_normaliser.normalize(I=T(self.image), stains=True)
-        self.image = PIL.Image.fromarray(np.uint8(norm.numpy())).convert('RGB')
+        self.image = PIL.Image.fromarray(np.uint8(norm.numpy())).convert("RGB")
 
-    def save(self):
+    def save(self) -> None:
         self.image.save("TCGA-AA-3516.png")
 
 
 if __name__ == "__main__":
-    preprocess = PreprocessingSVS('TCGA-AA-3516.svs')
+    preprocess = PreprocessingSVS("TCGA-AA-3516.svs")
     preprocess.resize_to_target_mpp()
     preprocess.crop()
     preprocess.normalise()
     preprocess.save()
-
-
