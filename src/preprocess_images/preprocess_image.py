df314309836b5c44c267bc2bbafe5ec863f8a562
kksniak <kksniak@gmail.com>
2022-11-24 15:08:12 +0100

    fix none path

--- src/preprocess_images/preprocess_image.py
+++ src/preprocess_images/preprocess_image.py
@@ -46,19 +46,20 @@ def filter_shape(data_path, shape):
 
 class PreprocessingSVS:
     def __init__(self, image_path=None, target_path=None) -> None:
+        if not image_path:
+            self.if_svs = False
+            print(
+                "Prepocessing without file, manually set self.image and self.image_dim"
+            )
+            return
+            
         self.image_path = image_path
         if not target_path:
             self.target_path = image_path.split(".")[0] + "_processed.tif"
         else:
             self.target_path = target_path
 
-        if not image_path:
-            self.if_svs = False
-            print(
-                "Prepocessing without file, manually set self.image and self.image_dim"
-            )
-
-        elif image_path.split(".")[-1] == "svs":  # read slide
+        if image_path.split(".")[-1] == "svs":  # read slide
             self.if_svs = True
             slide = openslide.OpenSlide(image_path)
 
@@ -124,7 +125,7 @@ if __name__ == "__main__":
     ## big image preprocessing (Daniel)
     preprocess = PreprocessingSVS("TCGA-AA-3516.svs")
     preprocess.resize_to_target_mpp()
-    preprocess.crop()
+    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
     preprocess.normalise()
     preprocess.save()
 
