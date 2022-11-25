b6d0ce96710fa6a49b19b231da2985c8337a8ca9
Frithiof Ekstroem <frithiof.ekstrom@gmail.com>
2022-11-25 12:58:33 +0100

    Add surivial dataset

--- src/preprocessing/create_dfs.py
+++ src/preprocessing/create_dfs.py
@@ -1,10 +1,16 @@
+import os
+import pickle
+
 import pandas as pd
 import numpy as np
+
+TABULAR_PATH = "data/TCGA_MEASUREMENTS.xlsx"
+SLIDES_PATH = "data/TCGA_processed"
 
 if __name__ == "__main__":
     np.random.seed(1)
 
-    df = pd.read_excel("data/TCGA_MEASUREMENTS.xlsx")
+    df = pd.read_excel(TABULAR_PATH)
 
     # Exclude columns that are manually labeled by experts
     cols = [
@@ -25,15 +31,29 @@ if __name__ == "__main__":
     ]
     df = df[cols]
 
-    n = df.shape[0]
-    perm = np.random.permutation(n)
+    patient_ids = df["ID"].to_numpy()
+    slide_filenames = os.listdir(SLIDES_PATH)
+    patient_slide_map = {}
+    for patient_id in patient_ids:
+        matching_slides = [
+            name for name in slide_filenames if name.startswith(patient_id)
+        ]
+        if len(matching_slides) == 0:
+            continue
+        patient_slide_map[patient_id] = [
+            os.path.join(SLIDES_PATH, name) for name in matching_slides
+        ]
+    df = df[df["ID"].isin(patient_slide_map.keys())].reset_index(drop=True)
 
     df["gender"] = df["gender"].replace({"male": 0, "female": 1})
     df["years_to_birth"] = df["years_to_birth"].fillna(-1)
 
-    X = df.loc[:, ~df.columns.isin(["days_to_event", "vital_status", "ID"])]
-    y = df[["vital_status", "days_to_event"]]
-    y["vital_status"] = df[["vital_status"]].astype(bool)
+    X = df.loc[:, ~df.columns.isin(["days_to_event", "vital_status"])]
+    y = df[["ID", "vital_status", "days_to_event"]]
+    y = y.astype({'vital_status': bool})
+
+    n = df.shape[0]
+    perm = np.random.permutation(n)
 
     X_train = X[X.index.isin(perm[: int(0.8 * n)])]
     X_test = X[~X.index.isin(perm[: int(0.8 * n)])]
@@ -44,3 +64,6 @@ if __name__ == "__main__":
     y_train.to_pickle("data/TCGA_y_train.pkl")
     X_test.to_pickle("data/TCGA_X_test.pkl")
     y_test.to_pickle("data/TCGA_y_test.pkl")
+
+    with open("data/TCGA_patient_slide_map.pkl", "wb") as f:
+        pickle.dump(patient_slide_map, f)
