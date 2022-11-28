3145102e31da544940cdf0d90762494e33384123
kksniak <kksniak@gmail.com>
2022-11-28 17:16:19 +0100

    segmentation plots

---
+++ src/postprocess_segmentation/plot_segmentation_maps.py
@@ -0,0 +1,61 @@
+import os
+
+import numpy as np
+import openslide
+import matplotlib
+from segmentation_mask_overlay import overlay_masks
+
+LEVEL = 1
+
+
+class plotSegmentation:
+    def __init__(
+        self,
+        original_folder: str,
+        source_folder: str,
+        target_folder: str,
+    ):
+        self.original_folder = original_folder
+        self.source_folder = source_folder
+        self.target_folder = target_folder
+        self.cmap = matplotlib.colors.ListedColormap(
+            ["orange", "cyan", "green", "red", "blue"]
+        )
+        self.__plot_and_save()
+
+    def __plot_and_save(self):
+        for _, filename in enumerate(os.listdir(self.original_folder)):
+            if filename.split(".")[-1] == "svs":
+                print("image ", filename)
+
+                slide = openslide.OpenSlide(self.original_folder + "/" + filename)
+                original_dimensions = slide.level_dimensions[1]
+                image = slide.read_region(
+                    (0, 0), LEVEL, slide.level_dimensions[LEVEL]
+                ).convert("RGB")
+                del slide
+
+                for _, source_filename in enumerate(os.listdir(self.source_folder)):
+                    if source_filename.split("_")[0] == filename.split(".")[0]:
+                        seg_matrix = np.load(self.source_folder + "/" + source_filename)
+                        mask = (
+                            np.arange(seg_matrix.max() + 1) == seg_matrix[..., None]
+                        ).astype(bool)
+                        fig = overlay_masks(
+                            image, mask, mask_alpha=0.5, mpl_colormap=self.cmap
+                        )
+                        fig.savefig(
+                            self.target_folder
+                            + "/"
+                            + source_filename.split(".")[0]
+                            + ".png",
+                            dpi=600,
+                        )
+
+
+if __name__ == "__main__":
+    plotSegmentation(
+        "./WSS2-v1/test",
+        "./WSS2_nonoverlapping_segmentation",
+        "./WSS2_nonoverlapping_segmentation_plots",
+    )
